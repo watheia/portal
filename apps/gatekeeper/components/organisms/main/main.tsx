ec0a585d82cc97514c7f2b04994bd1702af6dee8
Aaron R Miller <amiller@watheia.org>
2021-08-24 02:10:09 +0000

    Initial Commit

--- apps/gatekeeper/components/organisms/main/main.tsx
+++ apps/gatekeeper/components/organisms/main/main.tsx
@@ -212,7 +212,7 @@ export default function Main() {
                   href="#"
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
                 >
-                  Live demo
+                  Request demo
                 </a>
               </div>
             </div>
