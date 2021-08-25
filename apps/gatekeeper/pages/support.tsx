38dab991c0aaf5dca32dc9ba4c539cd4957a851e
Aaron R Miller <amiller@watheia.org>
2021-08-25 19:44:20 +0000

    adjust font colors

--- apps/gatekeeper/pages/support.tsx
+++ apps/gatekeeper/pages/support.tsx
@@ -230,10 +230,12 @@ export function About(props) {
               </div>
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                  <span className="block text-white">Take control of your</span>
-                  <span className="block text-ai-aqua-200">customer support</span>
+                  <span className="block text-white">Expert support</span>
+                  <span className="block text-secondary text-3xl">
+                    when you need it most
+                  </span>
                 </h1>
-                <p className="mt-6 max-w-lg mx-auto text-center text-xl text-ai-aqua-200 sm:max-w-3xl">
+                <p className="mt-6 max-w-lg mx-auto text-center text-xl text-secondary sm:max-w-3xl">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
                   cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
                 </p>
