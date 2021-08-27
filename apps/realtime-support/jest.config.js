c1dcf978d3edf030ad08ac5fcc1b43fa39fc0049
Aaron R Miller <amiller@watheia.org>
2021-08-27 08:23:23 +0000

    generate app stubs

---
+++ apps/realtime-support/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: "realtime-support",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/apps/realtime-support"
+}
