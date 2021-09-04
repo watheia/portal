ef78aebfcb08c96a3c0e6d9d756e72d0e88e44d2
Aaron R Miller <amiller@watheia.org>
2021-09-04 17:46:07 +0000

    update landing page

--- apps/gatekeeper/pages/index.tsx
+++ apps/gatekeeper/pages/index.tsx
@@ -41,7 +41,7 @@ import { ChevronRightIcon, ExternalLinkIcon } from "@heroicons/react/solid"
 import { Facebook, Github, Instagram, Twitter } from "@waweb/gfx"
 import { Page } from "@waweb/layout"
 import { useRouter } from "next/router"
-import { META_DESCRIPTION } from "@waweb/context"
+import { META_DESCRIPTION, MISSION_STATEMENT } from "@waweb/context"
 
 const navigation = [
   { name: "Product", href: "#" },
@@ -196,28 +196,31 @@ const footerNavigation = {
 }
 
 const HeroSection = () => (
-  <section className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+  <section className="pt-10 bg-shark-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
     <div className="mx-auto max-w-7xl lg:px-8">
       <div className="lg:grid lg:grid-cols-2 lg:gap-8">
         <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
           <div className="lg:py-24">
             <a
               href="#"
-              className="inline-flex items-center text-shark-50 bg-shark-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
+              className="inline-flex items-center text-shark-50 bg-shark-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-shark-200"
             >
               <span className="px-3 py-0.5 text-shark-50 text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-aqua-500 to-regal-400 rounded-full">
                 We&apos;re hiring
               </span>
               <span className="ml-4 text-sm text-secondary-2">Visit our careers page</span>
-              <ChevronRightIcon className="ml-2 w-5 h-5 text-gray-500" aria-hidden="true" />
+              <ChevronRightIcon
+                className="ml-2 w-5 h-5 text-shark-500"
+                aria-hidden="true"
+              />
             </a>
             <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-shark-50 sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
               <span className="block">A better way to</span>
-              <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-aqua-200 to-regal-400 sm:pb-5">
+              <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-aqua-200 to-aqua-500 sm:pb-5">
                 ship web apps
               </span>
             </h1>
-            <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+            <p className="text-base text-shark-300 sm:text-xl lg:text-lg xl:text-xl">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem
               cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.
             </p>
@@ -232,19 +235,19 @@ const HeroSection = () => (
                       id="email"
                       type="email"
                       placeholder="Enter your email"
-                      className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-regal-400 focus:ring-offset-gray-900"
+                      className="block w-full px-4 py-3 rounded-md border-0 text-base text-shark-900 placeholder-shark-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-regal-400 focus:ring-offset-shark-900"
                     />
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <button
                       type="submit"
-                      className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-aqua-500 to-regal-400 text-shark-50 font-medium hover:from-aqua-600 hover:to-regal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-regal-400 focus:ring-offset-gray-900"
+                      className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-aqua-500 to-regal-400 text-shark-50 font-medium hover:from-aqua-600 hover:to-regal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-regal-400 focus:ring-offset-shark-900"
                     >
                       Request login
                     </button>
                   </div>
                 </div>
-                <p className="mt-3 text-sm text-gray-300 sm:mt-4">
+                <p className="mt-3 text-sm text-shark-300 sm:mt-4">
                   Your privacy is important to us. Please read our{" "}
                   <a href="/term-and-conditions" className="font-medium text-shark-50">
                     terms and conditions
@@ -271,16 +274,16 @@ const HeroSection = () => (
 )
 
 const ServerlessSection = () => (
-  <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
+  <div className="relative bg-shark-50 pt-16 sm:pt-24 lg:pt-32">
     <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
       <div>
-        <h2 className="text-base font-semibold tracking-wider text-regal-600 uppercase">
+        <h2 className="text-base font-semibold tracking-wider text-aqua-600 uppercase">
           Serverless
         </h2>
-        <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+        <p className="mt-2 text-3xl font-extrabold text-shark-900 tracking-tight sm:text-4xl">
           No server? No problem.
         </p>
-        <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+        <p className="mt-5 max-w-prose mx-auto text-xl text-shark-500">
           Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis
           quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
         </p>
@@ -299,13 +302,13 @@ const ServerlessSection = () => (
 const FeaturesSection = () => (
   <div className="relative bg-shark-50 py-16 sm:py-24 lg:py-32">
     <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-      <h2 className="text-base font-semibold tracking-wider text-regal-600 uppercase">
+      <h2 className="text-base font-semibold tracking-wider text-aqua-600 uppercase">
         Deploy faster
       </h2>
-      <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+      <p className="mt-2 text-3xl font-extrabold text-shark-900 tracking-tight sm:text-4xl">
         Everything you need to deploy your app
       </p>
-      <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+      <p className="mt-5 max-w-prose mx-auto text-xl text-shark-500">
         Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis
         nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
       </p>
@@ -313,17 +316,17 @@ const FeaturesSection = () => (
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {features.map((feature) => (
             <div key={feature.name} className="pt-6">
-              <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div className="flow-root bg-shark-50 rounded-lg px-6 pb-8">
                 <div className="-mt-6">
                   <div>
                     <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-aqua-500 to-regal-400 rounded-md shadow-lg">
                       <feature.icon className="h-6 w-6 text-shark-50" aria-hidden="true" />
                     </span>
                   </div>
-                  <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                  <h3 className="mt-8 text-lg font-medium text-shark-900 tracking-tight">
                     {feature.name}
                   </h3>
-                  <p className="mt-5 text-base text-gray-500">{feature.description}</p>
+                  <p className="mt-5 text-base text-shark-500">{feature.description}</p>
                 </div>
               </div>
             </div>
@@ -335,7 +338,7 @@ const FeaturesSection = () => (
 )
 
 const TestimonialSection = () => (
-  <div className="pb-16 bg-gradient-to-r from-aqua-500 to-regal-600 lg:pb-0 lg:z-10 lg:relative">
+  <div className="pb-16 bg-gradient-to-r from-aqua-500 to-regal-400 lg:pb-0 lg:z-10 lg:relative">
     <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
       <div className="relative lg:-my-8">
         <div
@@ -364,15 +367,11 @@ const TestimonialSection = () => (
               >
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
-              <p className="mt-6 text-2xl font-medium text-shark-50">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla
-                vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc
-                urna, sit.
-              </p>
+              <p className="mt-6 text-2xl font-medium text-shark-50">{MISSION_STATEMENT}</p>
             </div>
             <footer className="mt-6">
-              <p className="text-base font-medium text-shark-50">Judith Black</p>
-              <p className="text-base font-medium text-regal-100">CEO at PureInsights</p>
+              <p className="font-medium text-shark-50">Aaron R Miller</p>
+              <p className="font-medium text-regal-100">CTO at Watheia Labs</p>
             </footer>
           </blockquote>
         </div>
@@ -382,18 +381,17 @@ const TestimonialSection = () => (
 )
 
 const FeaturedPostsSection = () => (
-  <div className="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
+  <div className="relative bg-shark-50 py-16 sm:py-24 lg:py-32">
     <div className="relative">
       <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-        <h2 className="text-base font-semibold tracking-wider text-regal-600 uppercase">
+        <h2 className="text-base font-semibold tracking-wider text-aqua-600 uppercase">
           Learn
         </h2>
-        <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-          Helpful Resources
+        <p className="mt-2 text-3xl font-extrabold text-shark-900 tracking-tight sm:text-4xl">
+          Case Studies
         </p>
-        <p className="mt-5 mx-auto max-w-prose text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis
-          quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
+        <p className="mt-5 mx-auto max-w-prose text-xl text-shark-500">
+          Our perspectives on business, technology, design, and more.
         </p>
       </div>
       <div className="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
@@ -410,8 +408,8 @@ const FeaturedPostsSection = () => (
                   </a>
                 </p>
                 <a href={post.href} className="block mt-2">
-                  <p className="text-xl font-semibold text-gray-900">{post.title}</p>
-                  <p className="mt-3 text-base text-gray-500">{post.preview}</p>
+                  <p className="text-xl font-semibold text-shark-900">{post.title}</p>
+                  <p className="mt-3 text-base text-shark-500">{post.preview}</p>
                 </a>
               </div>
               <div className="mt-6 flex items-center">
@@ -425,12 +423,12 @@ const FeaturedPostsSection = () => (
                   </a>
                 </div>
                 <div className="ml-3">
-                  <p className="text-sm font-medium text-gray-900">
+                  <p className="text-sm font-medium text-shark-900">
                     <a href={post.author.href} className="hover:underline">
                       {post.author.name}
                     </a>
                   </p>
-                  <div className="flex space-x-1 text-sm text-gray-500">
+                  <div className="flex space-x-1 text-sm text-shark-500">
                     <time dateTime={post.datetime}>{post.date}</time>
                     <span aria-hidden="true">&middot;</span>
                     <span>{post.readingLength} read</span>
@@ -446,8 +444,8 @@ const FeaturedPostsSection = () => (
 )
 
 const CtaSection = () => (
-  <div className="relative bg-gray-900">
-    <div className="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+  <div className="relative bg-shark-900">
+    <div className="relative h-56 bg-aqua-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
       <img
         className="w-full h-full object-cover"
         src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100"
@@ -460,13 +458,13 @@ const CtaSection = () => (
     </div>
     <div className="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
       <div className="md:ml-auto md:w-1/2 md:pl-10">
-        <h2 className="text-base font-semibold uppercase tracking-wider text-gray-300">
+        <h2 className="text-base font-semibold uppercase tracking-wider text-shark-300">
           Award winning support
         </h2>
         <p className="mt-2 text-shark-50 text-3xl font-extrabold tracking-tight sm:text-4xl">
           We’re here to help
         </p>
-        <p className="mt-3 text-lg text-gray-300">
+        <p className="mt-3 text-lg text-shark-300">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus
           etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue.
           Aliquet amet volutpat quisque ut interdum tincidunt duis.
@@ -475,11 +473,11 @@ const CtaSection = () => (
           <div className="inline-flex rounded-md shadow">
             <a
               href="#"
-              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-shark-50 hover:bg-gray-50"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-shark-900 bg-shark-50 hover:bg-shark-50"
             >
               Visit the help center
               <ExternalLinkIcon
-                className="-mr-1 ml-3 h-5 w-5 text-gray-400"
+                className="-mr-1 ml-3 h-5 w-5 text-shark-400"
                 aria-hidden="true"
               />
             </a>
@@ -501,6 +499,7 @@ const Index = (props) => {
       <HeroSection />
       <ServerlessSection />
       <FeaturesSection />
+      <TestimonialSection />
       <FeaturedPostsSection />
       <CtaSection />
     </Page>
