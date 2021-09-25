86a00f2431825c59389f2c6e37f597db5e3e83da
Aaron R Miller <amiller@watheia.org>
2021-09-24 17:02:20 -0700

    generate theme project

---
+++ libs/theme/src/lib/Theme.tsx
@@ -0,0 +1,14 @@
+import "./Theme.module.css"
+
+/* eslint-disable-next-line */
+export interface ThemeProps {}
+
+export function Theme(props: ThemeProps) {
+  return (
+    <div>
+      <h1>Welcome to Theme!</h1>
+    </div>
+  )
+}
+
+export default Theme
